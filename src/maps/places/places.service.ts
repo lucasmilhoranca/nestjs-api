c8f4fbd6aa06ae7bbca76e97a7ac93d4ba23339e
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2024-12-12 17:59:31 -0300

    initial commit

---
+++ src/maps/places/places.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import {
+  Client as GoogleMapsClient,
+  PlaceInputType,
+} from '@googlemaps/google-maps-services-js';
+
+@Injectable()
+export class PlacesService {
+  constructor(private googleMapsClient: GoogleMapsClient) {}
+  async findPlaces(text: string) {
+    const { data } = await this.googleMapsClient.findPlaceFromText({
+      params: {
+        input: text,
+        inputtype: PlaceInputType.textQuery,
+        fields: ['place_id', 'formatted_address', 'geometry', 'name'],
+        key: '',
+      },
+    });
+
+    return data;
+  }
+}
